5313716821d5078d7ebf2f67d4b15c7ddfeb95a7
Jan-Paul Wass <paul@wass-media.com>
2025-10-10 15:22:54 +0200

    Move CLI menu flow to main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+"""Run the command line interface to manage a movie database."""
+from movies import (
+    clear_screen,
+    show_menu,
+    get_user_choice,
+    say_bye,
+    execute_user_choice,
+    wait_for_enter_key
+)
+
+while True:
+    clear_screen()
+    show_menu()
+    user_choice = get_user_choice()
+    # Rebuild the menu with highlighted menu entry.
+    # This only works for numbers.
+    if user_choice.isdigit():
+        clear_screen()
+        show_menu(user_choice)
+    # Option to exit the program.
+    if user_choice == "0":
+        say_bye()
+        break
+    # Execute the selected action.
+    if execute_user_choice(user_choice):
+        pass
+    # Intentionally pause after every action,
+    # to allow for reading of information or error messages.
+    wait_for_enter_key()
--- movies.py
+++ movies.py
@@ -582,26 +582,8 @@ def say_bye():
 
 
 def main():
-    """Run a command line interface to manage a movie database."""
-    while True:
-        clear_screen()
-        show_menu()
-        user_choice = get_user_choice()
-        # Rebuild the menu with highlighted menu entry.
-        # This only works for numbers.
-        if user_choice.isdigit():
-            clear_screen()
-            show_menu(user_choice)
-        # Option to exit the program.
-        if user_choice == "0":
-            say_bye()
-            break
-        # Execute the selected action.
-        if execute_user_choice(user_choice):
-            pass
-        # Intentionally pause after every action,
-        # to allow for reading of information or error messages.
-        wait_for_enter_key()
+    """Main function for testing when running the script under main."""
+    pass
 
 
 if __name__ == "__main__":
